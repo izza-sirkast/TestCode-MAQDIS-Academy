ce55f2a42941895e6ba6fabc4df19a7c9a9e990f
izza-sirkast <dawlaizza405@gmail.com>
2025-06-25 06:27:14 +0700

    Tugas 2: mengabungkan kata

---
+++ Tugas 2/type.js
@@ -0,0 +1,8 @@
+const variable1 = "MAQDIS";
+const variable2 = "Academy";
+const variable3 = "id";
+const variable4 = "awesome";
+
+const gabungan = variable1 + " " + variable2 + " " + variable3 + " " + variable4;
+
+console.log(gabungan);
