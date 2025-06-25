5d1f8dd0b94d55ca5e57b1b40b3e6be64c6ddfa7
izza-sirkast <dawlaizza405@gmail.com>
2025-06-25 07:03:20 +0700

    Tugas 4: Looping

---
+++ Tugas 4/looping.js
@@ -0,0 +1,11 @@
+// LOOPING PERTAMA
+console.log("LOOPING PERTAMA");
+for (let i = 2; i <= 20; i += 2) {
+    console.log(i + " - I love coding");
+}
+
+// LOOPING KEDUA
+console.log("LOOPING KEDUA");
+for (let i = 20; i >= 2; i -= 2) {
+    console.log(i + " - I will become a mobile developer");
+}
