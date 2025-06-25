89e9378cc572f102b5f810c55d0a657e78309a5c
izza-sirkast <dawlaizza405@gmail.com>
2025-06-25 07:30:32 +0700

    api-sederhana: method GET pada route /hapus untuk mengembalikan respons hapus data berupa JSON

--- api-sederhana/index.js
+++ api-sederhana/index.js
@@ -23,6 +23,26 @@ app.post('/hapus', (req, res) => {
     })
 })
 
+
+// gunakan query parameters mengirimkan id_setoran, id_user, dan id_juz data yang mau dihapus
+// contoh: http://localhost:3000/hapus?id_setoran=20&id_user=21&id_juz=14
+app.get('/hapus', (req, res) => {
+    const { id_setoran, id_user, id_juz } = req.query;
+
+    if (!id_setoran || !id_user || !id_juz) {
+        return res.status(400).json({
+            error: "id_setoran, id_user, and id_juz are required in query parameters"
+        });
+    }
+
+    res.status(200).json({
+        message: "berhasil",
+        id_setoran,
+        id_user,
+        id_juz
+    })
+})
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 })
